21ea28edc82c403818fd6b0483f0bbd39bb83a85
Peggia <juraj.pejnovic@gmail.com>
2019-01-16 04:11:07 +0100

    Add solution.py

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
+import time
+
 from loadInstance import loadInstance
 from evaluators import evaluator1, evaluator2
+from solution import addToSolutions, outputSolution
 
 
 def allowed(node, vehicles, lanes, vehicleLaneMatrix):
+    return False
 
 
-def exploreNode(node, vehicles, lanes, vehicleLaneMatrix, solutions):
+def exploreNode(node, vehicles, lanes, vehicleLaneMatrix, solutions, nodesToVisit):
     if not allowed(node, vehicles, lanes, vehicleLaneMatrix):
         return
 
-    print("allowed")
+
+def someTimeLeft(unlimited, startTime, timeAllowed):
+    if unlimited:
+        return True
+
+    return ((time.time() - startTime) < timeAllowed)
 
 
 def main():
-    sortedVehicles, lanes, vehicleLaneMatrix = loadInstance("instanca1.txt")
+    vehicles, lanes, vehicleLaneMatrix = loadInstance("instanca1.txt")
+
+    # make a node here
+    firstNode = {
+
+    }
+
+    nodesToVisit = []
+    solutions = []
+
+    nodesToVisit.append(firstNode)
+
+    startTime = time.time()
+    time.clock()
+    while nodesToVisit and someTimeLeft(False, startTime, 60):
+        workingNode = nodesToVisit.pop(0)
+        exploreNode(workingNode, vehicles, lanes,
+                    vehicleLaneMatrix, solutions, nodesToVisit)
+
+    outputSolution(solutions)
 
 
 if __name__ == "__main__":
---
+++ solution.py
@@ -0,0 +1,11 @@
+def addToSolutions(node, solutions):
+    solutions.append(node)
+
+
+# WIP - treba naci najbolji solution i isprintati ga, preovjerava se po ona dva kriterija
+def outputSolution(solutions):
+    writeSolution(solutions)
+
+
+def writeSolution(solution):
+    print(solution)
