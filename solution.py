d3c391e0236f0b40d92f91835f4633c96c9dba2f
Peggia <juraj.pejnovic@gmail.com>
2019-01-16 23:35:32 +0100

    Add stuff

--- solution.py
+++ solution.py
@@ -1,18 +1,34 @@
-
+from node import evaluateNode
 
 # WIP
 # adds the node to solutions, possibly sorting them
+
+
 def addToSolutions(node, solutions):
+    evaluateNode(node)
     solutions.append(node)
 
 
 # WIP
 # finds best solution and calls writeSolution
 def outputSolution(solutions):
-    writeSolution(solutions)
+    if len(solutions) == 0:
+        print("no solution found")
+        return
+
+    currentBestSolution = solutions[0]
+    currentMaxValue = currentBestSolution["eval2"]/currentBestSolution["eval1"]
+
+    for sol in solutions:
+        workingValue = sol["eval2"]/sol["eval1"]
+        if currentMaxValue < workingValue:
+            currentMaxValue = workingValue
+            currentBestSolution = sol
+
+    writeSolution(currentBestSolution)
 
 
 # WIP
 # prints out the solution in the required format
-def writeSolution(solution):
-    print(solution)
+def writeSolution(node):
+    print(node)
